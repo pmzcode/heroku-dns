fdf1946cfa47cf0afe7de79e9716d3ec6941f6aa
Egor Pomazai <pmz-work@yandex.ru>
2017-05-12 13:37:18 +0300

    ds

--- context/db.js
+++ context/db.js
@@ -4,8 +4,8 @@ module.exports = (Sequelize, config) => {
     const dbConfig = process.env.PMZ_PROJECT === 'production' ? config.prod : config.db;
 
     const options = {
-        host: config.db.host,
-        dialect: config.db.dialect,
+        host: dbConfig.host,
+        dialect: dbConfig.dialect,
         logging: false,
         define: {
             timestamps: true,
@@ -18,7 +18,7 @@ module.exports = (Sequelize, config) => {
         }
     };
 
-    const sequelize = new Sequelize(config.db.name, config.db.user, config.db.password, options);
+    const sequelize = new Sequelize(dbConfig.name, dbConfig.user, dbConfig.password, options);
     const User = require('../models/user')(Sequelize, sequelize);
     const Domain = require('../models/domain')(Sequelize, sequelize);
     const UserDomains = require('../models/userDomains')(Sequelize, sequelize);
